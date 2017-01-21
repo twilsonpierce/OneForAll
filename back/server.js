5b3aab996d664d91819cb6739c3249baf530a307
Ruben Gil <gil.r419@gmail.com>
2017-01-21 14:50:24 -0500

    initial setup

---
+++ back/server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+
+app.use(express.static(path.join(__dirname, '../front/bundle')))
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../front/index.html'))
+})
+
+app.listen(3000)
---
+++ front/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+const App = (props) => (
+  <div>
+    <h1>SETUP</h1>
+  </div>
+)
+
+render(<App />, document.getElementById('app'));
