62a254e10c496de2237d3c9c93999a772ab223ca
Dee Sterling <darlenesterling@gmail.com>
2017-01-21 20:58:59 -0500

    create css

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   context: __dirname,
   entry: "./front/App.jsx",
   output: {
-    path: path.join(__dirname, '/front/bundle'),
+    path: path.join(__dirname, '/front/public/bundle'),
     filename: "bundle.js"
   },
   module: {
