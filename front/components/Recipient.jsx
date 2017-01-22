3ab737a26adfc5869ae3ccd4721b90b169c4d5c6
Joshua Fermin <Joshuaf91@gmail.com>
2017-01-22 17:31:53 -0500

    closes #19

---
+++ front/components/Recipient.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Recipient = React.createClass({
+  render(){
+    return(
+      <div>
+        hello
+      </div>
+      )
+  }
+})
+
+export default Recipient;
--- front/reducers/reducer.js
+++ front/reducers/reducer.js
@@ -9,25 +9,31 @@ const reducer = (state = defaultState, action) => {
     case REGISTER:
       let newUserList = [...state.users];
       newUserList.push(action.data);
+      let id = newUserList.length-1
+      console.log(action.data);
+      if(action.data.type === "donor"){
+        browserHistory.push(`/donor/${id}`);
+      }else {
+        //this a place holder for recipient
+        browserHistory.push(`/recipient/${id}`);
+      }
 
-      return Object.assign({}, state, {users: newUserList, userId});
+      return Object.assign({}, state, {users: newUserList, loggedIn:action.data});
     case LOGIN:
       let user
       state.users.forEach((ele,idx)=>{
-        console.log(ele)
-        console.log(action.data)
+        
         if(ele.email == action.data.email && ele.password == action.data.password){
 
-          if(ele.type === "doner"){
+          if(ele.type === "donor"){
             browserHistory.push(`/donor/${idx}`);
           }else {
             //this a place holder for recipient
-            browserHistory.push(`/something/${idx}`);
+            browserHistory.push(`/recipient/${idx}`);
           }
         }
       })
-
-      Object.assign({}, state, {loggedIn: user})
+      return Object.assign({}, state, {loggedIn: user})
     case DONATE:
       return Object.assign({}, state, {donation, numOfItems});
     case SUBMIT_DONATION:
