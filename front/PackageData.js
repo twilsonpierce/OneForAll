ea1b697f2295bd96e0653d9a5a7b2e09353a9be7
Tionna Wilson-Pierce <tionna.wilsonpierce@gmail.com>
2017-01-21 20:36:24 -0500

    finished data file for packages

---
+++ front/PackageData.js
@@ -0,0 +1,31 @@
+export default {
+	"packageOne": [
+		{
+			"Boxes of Breakfast Bars": 3,
+			"Boxes of Rice": 3,
+			"Canned Soup": 3,
+			"Canned of Vegetables(All)": 3,
+			"Canned of Spaghetti Sauce": 3,
+		}
+	]
+	"packageTwo": [
+		{
+			"Large Juices": 3,
+			"Boxes of Oatmeal": 3,
+			"Boxes of Dry Macaroni and Cheese": 3,
+			"Canned of Vegetables(All)": 3,
+			"Canned of Pink Salmon": 3,
+		}
+	]
+	"packageOThree": [
+		{
+			"Peanut Butter and Jelly": 3,
+			"Beans(Canned or Dry)": 3,
+			"Canned Tuna": 3,
+			"Canned of Vegetables(All)": 3,
+			"Fruit(Dry or Canned)": 3,
+		}
+	]
+}
+
+
